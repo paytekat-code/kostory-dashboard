7123c25ba814ca8e13333f65529984cb37f77a3d
paytekat-code <paytekat@gmail.com>
2025-12-11 10:25:54 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAhN2a4m6PkTwFOvJ88TreD1lCERYJD7m0",
+  authDomain: "kostory-db.firebaseapp.com",
+  databaseURL: "https://kostory-db-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "kostory-db",
+  storageBucket: "kostory-db.appspot.com",
+  messagingSenderId: "447318101438",
+  appId: "1:447318101438:web:7aba8e16ccee69fd3c53def"
+};
