5d8bc8b174affd9cd6ca3092a354cf95f4d73ec6
paytekat-code <paytekat@gmail.com>
2025-12-12 10:25:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -807,11 +807,11 @@ window.showPenghuniList = async function() {
       let level = "";
       let warna = "";
 
-      if (tahun >= 5) { level = "Limit Sage"; warna = "#fbbf24"; }       // emas
-      else if (tahun >= 4) { level = "Sage"; warna = "#dc2626"; } // merah
-      else if (tahun >= 3) { level = "Emperor"; warna = "#92400e"; }    // coklat
-      else if (tahun >= 2) { level = "King"; warna = "#ec4899"; } // pink
-      else if (tahun >= 1) { level = "Elder"; warna = "#16a34a"; }   // hijau
+      if (tahun >= 5) { level = "Limit Sage🪽"; warna = "#fbbf24"; }       // emas
+      else if (tahun >= 4) { level = "Sage🧙‍♂️"; warna = "#dc2626"; } // merah
+      else if (tahun >= 3) { level = "Emperor🏰"; warna = "#92400e"; }    // coklat
+      else if (tahun >= 2) { level = "King👑"; warna = "#ec4899"; } // pink
+      else if (tahun >= 1) { level = "Elder🕯️"; warna = "#16a34a"; }   // hijau
       // kurang dari 1 tahun → tidak ditampilkan
 
       if (level) {
